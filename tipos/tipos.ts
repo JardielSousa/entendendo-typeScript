79f32f372e898554358ee7de7f84e49741ff7b6f
Jardiel Sousa <jardiel@infoway-pi.com.br>
2020-03-05 13:49:28 -0300

    Introdução

---
+++ tipos/tipos.ts
@@ -0,0 +1,22 @@
+let nome = "Jardiel";
+console.log(nome);
+// nome = 37;
+
+
+// numbers
+let idade: number = 38;
+idade = 38.9;
+// idade = "Jardiel";
+console.log(idade);
+
+// boolean
+let possuiHobbies: boolean = false;
+// possuiHobbies = 1;
+console.log(possuiHobbies);
+
+// Tipos explicitos
+let minhaidade: any;
+minhaidade = 37;
+console.log(typeof minhaidade);
+minhaidade = "38";
+console.log(typeof minhaidade);
